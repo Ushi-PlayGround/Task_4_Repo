8f30861476d6cbeda6c051bb25f547dd581f4f1f
Udari Rathnasiri <udarihashini@gmail.com>
2023-08-14 09:12:11 +0530

    Completed Algo-2

---
+++ Algo2.java
@@ -0,0 +1,22 @@
+public class Algo2 {
+    
+    public static void main(String[] args) {
+        
+        int[] nums = {-5,2,7,10,58,-7,8,23};
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int i = 0; i < nums.length-1; i++) {
+            if (max <= nums[i]){
+                max = nums[i];
+            }
+            if (min >= nums[i]){
+                min = nums[i];
+            }
+            
+        }
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+}
